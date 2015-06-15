fb54ff4e978174a23e2fe3a53a824a91704260b0
Elyahou <ittah.elyahou@gmail.com>
2015-06-16 02:31:36 +0300

    Handle flash messages

--- src/views/default/_layout/inner.blade.php
+++ src/views/default/_layout/inner.blade.php
@@ -15,6 +15,19 @@
 				<h1>{{{ $title }}}</h1>
 			</section>
 			<section class="content">
+				@if(Session::has('succes_message'))
+					<div class="callout callout-success">
+						<p><i class="icon fa fa-check"></i> 
+						   {{ Session::get('succes_message') }}.
+						</p>
+					</div>
+				@elseif(Session::has('error_message'))
+					<div class="callout callout-danger">
+						<p><i class="icon fa fa-warning"></i> 
+						   {{ Session::get('error_message') }}.
+						</p>
+					</div>
+				@endif
 				{!! $content !!}
 			</section>
 		</div>
